9e72c7be70eb6d4e68f2b1b4e945843833fa0c89
Lankreeni <niko.landgren@tuni.fi>
2023-01-17 08:53:49 +0200

    Helppo alv laskuri

---
+++ alvLaskuri.py
@@ -0,0 +1,12 @@
+# Alv laskuri k kaupan pojille joilla hankaluuksia
+# laskea alvia
+
+
+
+num1 = (float(0.1228070175438596))
+num2 = float(input("Anna alviton hinta: "))
+num3 = num1 * num2
+num4 = num2 - num3
+
+print("Alvin hinta tuotteesta on: ", num3)
+print("Alviton hinta tuotteelle on", num4)
